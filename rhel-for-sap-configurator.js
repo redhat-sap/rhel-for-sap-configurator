8d62342eace7b97a1be7ac536e95a7057d53307f
Bernd Finger <bfinger@redhat.com>
2025-12-22 10:41:56 +0100

    v1.16.0: SAP note 2378962: last HANA SPS05 rev is 059.20.
    Signed-off-by: Bernd Finger <bfinger@redhat.com>

--- rhel-for-sap-configurator.js
+++ rhel-for-sap-configurator.js
@@ -184,6 +184,8 @@
 //       Thu Sep 11 2025
 // v1.15.0: SAP note 2378962: last HANA SPS08 rev is 088.00, last HANA SPS07 rev is 079.07
 //       Mon Nov 03 2025
+// v1.16.0: SAP note 2378962: last HANA SPS05 rev is 059.20.
+//       Mon Dec 22 2025
 
 function displaySelections() { // used for debugging
    var elem = document.getElementsByName('sapSelect');
@@ -232,7 +234,7 @@ function displayResults() {
    };
    last_hana2_sps03 = "HANA 2.0 SPS03 rev 37.07";
    last_hana2_sps04 = "HANA 2.0 SPS04 rev 48.06";
-   last_hana2_sps05 = "HANA 2.0 SPS05 rev 59.19";
+   last_hana2_sps05 = "HANA 2.0 SPS05 rev 59.20";
    last_hana2_sps06 = "HANA 2.0 SPS06 rev 67.04";
    last_hana2_sps07 = "HANA 2.0 SPS07 rev 79.07";
    last_hana2_sps08 = "HANA 2.0 SPS08 rev 88.00";
